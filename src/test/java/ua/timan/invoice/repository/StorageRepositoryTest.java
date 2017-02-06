342bc2fb74ad5364a7cfe501eda6dc905311b7e5
mykhailo.pugach <satiricon@ukr.net>
2017-02-06 17:16:58 +0200

    подтягиваем данные для теста из json-а

--- src/test/java/ua/timan/invoice/repository/StorageRepositoryTest.java
+++ src/test/java/ua/timan/invoice/repository/StorageRepositoryTest.java
@@ -1,8 +1,7 @@
 package ua.timan.invoice.repository;
 
 import static org.junit.Assert.assertEquals;
-import static ua.timan.invoice.test.TestDataFactory.MAPPER;
-import static ua.timan.invoice.test.TestDataFactory.getFixture;
+import static ua.timan.invoice.test.TestDataFactory.createStorage;
 
 import java.io.IOException;
 
@@ -17,25 +16,22 @@ import ua.timan.invoice.test.AbstractSpringTest;
 @Slf4j
 public class StorageRepositoryTest extends AbstractSpringTest {
 
-	public static final int ID = 1;
+    private Storage entity;
 
-	private Storage entity;
+    @Autowired
+    private StorageRepository repository;
 
-	@Autowired
-	private StorageRepository repository;
+    @Before
+    public void setUp() throws IOException {
+        entity = createStorage();
+    }
 
-	@Before
-	public void setUp() throws IOException {
-		entity = (Storage) MAPPER.readValue(getFixture("Storage.json"), Storage.class);
-	}
-
-	@Test
-	public void shouldSaveAndGetStorageEntity() {
-		repository.save(entity);
-		Storage result = repository.findOne(ID);
-		assertEquals(entity, result);
-		log.info(result.toString());
-
-	}
+    @Test
+    public void shouldSaveAndGetStorageEntity() {
+        repository.save(entity);
+        Storage result = repository.findOne(entity.getId());
+        assertEquals(entity, result);
+        log.info(result.toString());
+    }
 
 }
--- src/test/java/ua/timan/invoice/test/TestDataFactory.java
+++ src/test/java/ua/timan/invoice/test/TestDataFactory.java
@@ -6,10 +6,12 @@ import static lombok.AccessLevel.PRIVATE;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 
 import lombok.NoArgsConstructor;
@@ -21,47 +23,39 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 @NoArgsConstructor(access = PRIVATE)
 public final class TestDataFactory {
 
-	public static final ObjectMapper MAPPER = new ObjectMapper();
+    public static final ObjectMapper MAPPER = new ObjectMapper();
 
-	public static final String FIXTURES_PATH = "fixtures" + File.separator;
+    public static final String FIXTURES_PATH = "fixtures" + File.separator;
 
-	static {
-		MAPPER.registerModule(new JSR310Module());
-	}
+    static {
+        MAPPER.registerModule(new JSR310Module());
+    }
 
-	private static final PodamFactory PODAM_FACTORY = new PodamFactoryImpl();
+    private static final PodamFactory PODAM_FACTORY = new PodamFactoryImpl();
 
-	public static InputStream getResource(String fileName) throws IOException {
-		return TestDataFactory.class.getClassLoader().getResourceAsStream(fileName);
-	}
+    public static InputStream getResource(String fileName) throws IOException {
+        return TestDataFactory.class.getClassLoader().getResourceAsStream(fileName);
+    }
 
-	public static InputStream getFixture(String fileName) throws IOException {
-		return getResource(FIXTURES_PATH + fileName);
-	}
+    public static InputStream getFixture(String fileName) throws IOException {
+        return getResource(FIXTURES_PATH + fileName);
+    }
 
-	public static PackingList createPackingList() {
-		return PODAM_FACTORY.manufacturePojo(PackingList.class);
-	};
+    public static PackingList createPackingList() {
+        return PODAM_FACTORY.manufacturePojo(PackingList.class);
+    };
 
-	public static List<PackingList> createPackingLists() {
-		return asList(createPackingList(), createPackingList());
-	};
+    public static List<PackingList> createPackingLists() {
+        return asList(createPackingList(), createPackingList());
+    };
 
-	public static List<Storage> createStorages() {
-		Storage storage1 = new Storage(1, "Склад №1 (Уборевича)");
-		Storage storage2 = new Storage(2, "Склад №2 (Вереснева)");
-		Storage store1 = new Storage(1, "ТП №1 (Симиренко, 25)");
-		List<Storage> list = new ArrayList<Storage>();
-		
-		list.add(storage1);
-		list.add(storage2);
-		list.add(store1);
+    public static List<Storage> createStorages() throws IOException {
+        return MAPPER.readValue(getFixture("Storages.json"),
+                CollectionType.construct(List.class, SimpleType.construct(Storage.class)));
+    }
 
-		return list;
-	}
-
-	public static Storage createStorage() {
-		return createStorages().get(0);
-	}
-
+    public static Storage createStorage() throws IOException {
+        List<Storage> storages = createStorages();
+        return storages.get(new Random().nextInt(storages.size()));
+    }
 }
