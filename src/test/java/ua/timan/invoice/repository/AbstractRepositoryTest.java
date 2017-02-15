693a740c3070f760118762e6fd07b13666bfab7d
mykhailo.pugach <satiricon@ukr.net>
2017-02-15 11:56:06 +0200

    загружаем тестовые данные в БД единожды для ВСЕХ тестов

--- src/test/java/ua/timan/invoice/repository/AbstractRepositoryTest.java
+++ src/test/java/ua/timan/invoice/repository/AbstractRepositoryTest.java
@@ -4,20 +4,13 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.collection.IsEmptyIterable.emptyIterable;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static ua.timan.invoice.test.TestDataFactory.getResourceAsString;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Random;
 
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
@@ -26,28 +19,12 @@ import ua.timan.invoice.domain.IdAware;
 import ua.timan.invoice.test.AbstractSpringTest;
 
 @Slf4j
-public abstract class AbstractRepositoryTest<E extends IdAware> extends AbstractSpringTest implements InitializingBean {
-
-    private static boolean testDataInserted = false;
-
-    @Autowired
-    protected DataSource dataSource;
+public abstract class AbstractRepositoryTest<E extends IdAware> extends AbstractSpringTest {
 
     @Autowired
     protected CrudRepository<E, Integer> repository;
 
     protected E entity;
-
-    @Override
-    public final void afterPropertiesSet() throws SQLException, IOException {
-        if (testDataInserted) {
-            return;
-        }
-        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
-            statement.execute(getResourceAsString("test-data.sql"));
-        }
-        testDataInserted = true;
-    }
 
     @Before
     public final void setUp() throws Exception {
