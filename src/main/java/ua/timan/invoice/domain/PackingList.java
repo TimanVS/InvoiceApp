0d8cea565a0c6918829bfbce63c43813b8e8b590
Mykhailo Pugach <satiricon@ukr.net>
2017-02-22 08:04:32 +0200

    починил OneToMany без создания дополнительной таблицы

--- src/main/java/ua/timan/invoice/domain/PackingList.java
+++ src/main/java/ua/timan/invoice/domain/PackingList.java
@@ -3,17 +3,19 @@ package ua.timan.invoice.domain;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ua.timan.invoice.converters.LocalDateAttributeConverter;
 
 @Data
 @NoArgsConstructor
@@ -24,12 +26,14 @@ public class PackingList implements IdAware {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Convert(converter = LocalDateAttributeConverter.class)
 	private LocalDate issueDate;
 	@ManyToOne
 	private Provider provider;
 	@ManyToOne
 	private Storage store;
-	@ManyToMany
+	@OneToMany
+	@JoinColumn(name = "PACKINGLIST_ID")
 	private List<PackingItem> items;
 	private String comment;
 
