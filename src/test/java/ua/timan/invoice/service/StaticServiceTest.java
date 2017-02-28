0f2baea8216a32b9619b51d9da633fc7a5b3916a
Sklyarov Aleksey <timanvs@gmail.com>
2017-02-28 10:13:41 +0200

    Дописал тесты на StaticService

--- src/test/java/ua/timan/invoice/service/StaticServiceTest.java
+++ src/test/java/ua/timan/invoice/service/StaticServiceTest.java
@@ -2,6 +2,7 @@ package ua.timan.invoice.service;
 
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.collection.IsEmptyIterable.emptyIterable;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
@@ -9,10 +10,13 @@ import java.util.List;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ua.timan.invoice.domain.Provider;
 import ua.timan.invoice.domain.Storage;
 import ua.timan.invoice.test.AbstractSpringTest;
 
 public class StaticServiceTest extends AbstractSpringTest {
+
+	public static final int EXISTEN_ID = 3;
 
 	@Autowired
 	private StaticService staticService;
@@ -21,6 +25,24 @@ public class StaticServiceTest extends AbstractSpringTest {
 	public void shouldGetAllStorages() {
 		List<Storage> list = staticService.getAllStorages();
 		assertThat(list, not(emptyIterable()));
+	}
+
+	@Test
+	public void shouldGetAllProviders() {
+		List<Provider> list = staticService.getAllProviders();
+		assertThat(list, not(emptyIterable()));
+	}
+
+	@Test
+	public void shouldGetStorage() {
+		Storage result = staticService.getStorage(EXISTEN_ID);
+		assertNotNull(result);
+	}
+
+	@Test
+	public void shouldGetProvider() {
+		Provider result = staticService.getProvider(EXISTEN_ID);
+		assertNotNull(result);
 	}
 
 }
