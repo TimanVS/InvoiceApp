13dde34ffe9f5a1d2163442d89919692e505e20a
Sklyarov Aleksey <timanvs@gmail.com>
2017-02-28 13:19:05 +0200

    JsonTests

--- src/test/java/ua/timan/invoice/domain/ProductJsonTest.java
+++ src/test/java/ua/timan/invoice/domain/ProductJsonTest.java
@@ -1,32 +1,5 @@
 package ua.timan.invoice.domain;
 
-import static org.junit.Assert.assertNotNull;
-import static ua.timan.invoice.test.TestDataFactory.MAPPER;
-import static ua.timan.invoice.test.TestDataFactory.createPodam;
-import static ua.timan.invoice.test.TestDataFactory.getFixture;
-
-import java.io.IOException;
-
-import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class ProductJsonTest {
-
-	@Test
-	public void shouldConvertProductToJson() throws JsonProcessingException {
-		String json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(createPodam(Product.class));
-		log.info(json);
-	}
-
-	@Test
-	public void shouldDeserializeProduct() throws IOException {
-		Product result = (Product) MAPPER.readValue(getFixture("Product.json"), Product.class);
-		log.info(result.toString());
-		assertNotNull(result);
-	}
+public class ProductJsonTest extends AbstractJsonTest<Product> {
 
 }
--- src/test/java/ua/timan/invoice/domain/StorageJsonTest.java
+++ src/test/java/ua/timan/invoice/domain/StorageJsonTest.java
@@ -1,31 +1,4 @@
 package ua.timan.invoice.domain;
 
-import static org.junit.Assert.assertNotNull;
-import static ua.timan.invoice.test.TestDataFactory.MAPPER;
-import static ua.timan.invoice.test.TestDataFactory.createPodam;
-import static ua.timan.invoice.test.TestDataFactory.getFixture;
-
-import java.io.IOException;
-
-import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class StorageJsonTest {
-
-	@Test
-	public void shouldConvertStorageToJson() throws JsonProcessingException {
-		String json = MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(createPodam(Storage.class));
-		log.info(json);
-	}
-
-	@Test
-	public void shouldDeserializeStorage() throws IOException {
-		Storage result = (Storage) MAPPER.readValue(getFixture("Storage.json"), Storage.class);
-		log.info(result.toString());
-		assertNotNull(result);
-	}
+public class StorageJsonTest extends AbstractJsonTest<Storage> {
 }
