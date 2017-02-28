1a5772c9da179a10cde4e57f806c0a739f1bd822
Sklyarov Aleksey <timanvs@gmail.com>
2017-02-28 17:21:26 +0200

    Тестдля ProductGroupRepository.findByName()

--- src/test/java/ua/timan/invoice/repository/ProductGroupRepositoryTest.java
+++ src/test/java/ua/timan/invoice/repository/ProductGroupRepositoryTest.java
@@ -1,14 +1,33 @@
 package ua.timan.invoice.repository;
 
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.collection.IsEmptyIterable.emptyIterable;
+import static org.junit.Assert.assertThat;
 import static ua.timan.invoice.test.TestDataFactory.createProductGroup;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import ua.timan.invoice.domain.ProductGroup;
 
 public class ProductGroupRepositoryTest extends AbstractRepositoryTest<ProductGroup> {
 
-    @Override
-    protected ProductGroup createEntity() throws Exception {
-        return createProductGroup();
-    }
+	@Override
+	protected ProductGroup createEntity() throws Exception {
+		return createProductGroup();
+	}
+
+	@Test
+	public void shouldFindProductGroupByName() {
+		String existentName = "Кондитерские изделия";
+		Iterable<ProductGroup> groups = ((ProductGroupRepository) repository).findByName(existentName);
+
+		assertThat(groups, not(emptyIterable()));
+		for (ProductGroup group : groups) {
+			String result = group.getName();
+			Assert.assertEquals(existentName, result);
+		}
+
+	}
 
 }
