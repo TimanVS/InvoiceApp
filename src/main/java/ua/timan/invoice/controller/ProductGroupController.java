55c4da4595c93fc7c804ff42ea739d863baae4b4
Sklyarov Aleksey <timanvs@gmail.com>
2017-03-03 22:46:04 +0200

    Исправил, казалось бы, ключевую ошибку. Но предыдущий коммит работал и с ней.

--- src/main/java/ua/timan/invoice/controller/ProductGroupController.java
+++ src/main/java/ua/timan/invoice/controller/ProductGroupController.java
@@ -15,38 +15,38 @@ import ua.timan.invoice.service.ProductService;
 
 @Controller
 public class ProductGroupController {
-	
+
 	private static final String ROOT_PATH = "/groups";
-	
+
 	@Autowired
 	private ProductService groupService;
-	
+
 	@RequestMapping
 	@ResponseBody
 	public List<ProductGroup> getAll() {
 		return groupService.getAllProductGroups();
 	}
-	
+
 	@RequestMapping(ROOT_PATH + "/{id}")
 	@ResponseBody
-	public ProductGroup getById(@PathVariable("id") int id){
+	public ProductGroup getById(@PathVariable("id") int id) {
 		return groupService.getProductGroup(id);
 	}
-	
+
 	@RequestMapping(ROOT_PATH + "/remove/{id}")
 	@ResponseBody
-	public void deleteById(@PathVariable("id") int id){
+	public void deleteById(@PathVariable("id") int id) {
 		groupService.deleteProductGroup(id);
 	}
-	
+
 	@RequestMapping(value = ROOT_PATH + "/add", method = RequestMethod.POST)
 	@ResponseBody
-	public ProductGroup addGroup(@ModelAttribute("group") ProductGroup group){
-		
-		if (!groupService.existsProduct(group.getId())){
+	public ProductGroup addGroup(@ModelAttribute("group") ProductGroup group) {
+
+		if (!groupService.existsProductGroup(group.getId())) {
 			groupService.createProductGroup(group);
 		}
-		
+
 		return groupService.updateProductGroup(group);
 	}
 
