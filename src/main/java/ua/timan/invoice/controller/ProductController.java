c6b44f9d1820acc63ae2116c4e9e6a4407841fdb
Sklyarov Aleksey <timanvs@gmail.com>
2017-03-14 11:53:51 +0200

    ProductController

--- src/main/java/ua/timan/invoice/controller/ProductController.java
+++ src/main/java/ua/timan/invoice/controller/ProductController.java
@@ -1,9 +1,15 @@
 package ua.timan.invoice.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,5 +27,29 @@ public class ProductController {
 	@ResponseBody
 	public List<Product> getAll() {
 		return productService.getAllProducts();
+	}
+
+	@RequestMapping(ROOT_PATH + "/{id}")
+	@ResponseBody
+	public Product getById(@PathVariable("id") int id) {
+		return productService.getProduct(id);
+	}
+
+	@RequestMapping(value = ROOT_PATH + "/{id}", method = DELETE)
+	@ResponseBody
+	public void deleteById(@PathVariable("id") int id) {
+		productService.deleteProduct(id);
+	}
+
+	@RequestMapping(value = ROOT_PATH, method = POST)
+	@ResponseBody
+	public Product add(@RequestBody Product product) {
+		return productService.createProduct(product);
+	}
+
+	@RequestMapping(value = ROOT_PATH, method = PUT)
+	@ResponseBody
+	public Product update(@RequestBody Product product) {
+		return productService.updateProduct(product);
 	}
 }
